41cd447c98bd61adf1ed65629ad15de6c0b92cc3
ddock4you <ddock4you@gmail.com>
2024-01-11 20:20:27 +0900

    clerk 인증 모듈 추가

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <>
-      <p className="text-3xl font-medium text-sky-700">Hello World</p>
-      <Button variant="link">버튼</Button>
-    </>
-  );
+    return (
+        <>
+            <UserButton afterSignOutUrl="/" />
+            <p className="text-3xl font-medium text-sky-700">Hello World</p>
+            <Button variant="link">버튼</Button>
+        </>
+    );
 }
